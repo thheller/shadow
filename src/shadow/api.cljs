0f3d51f22c2b68181fd9144ee5987092ea6cbc74
Thomas Heller <info@zilence.net>
2013-09-11 11:24:28 +0200

    cleanup html script api

--- src/shadow/api.cljs
+++ src/shadow/api.cljs
@@ -7,34 +7,57 @@
             [clojure.set :as set]
             [clojure.string :as str]))
 
-(defn run-init-function [fn-id args]
-  (let [queued-fn (goog/getObjectByName fn-id)]
+(defn run-script-tag
+  "a <script type=\"shadow/run\" data-module=\"mod-name\" data-fn=\"js-fn\">args</script> tag is meant to embed calls to javascript in html
+   instead of writing the javascript inline, we only define the call and its args + the location in the dom
+   we want to reference. this allows the javascript to be loaded as late as possible, avoids unknown reference errors,
+   does not litter the html with $(function() {}); and since a dom reference point is provided it makes it more
+   logical to reference dom elements via the server, no need to mess with id/class selectors.
+
+   script tags will be executed as soon as the js module is loaded (assuming it called module-ready), not on dom ready
+   which means it triggers earlier"
+  [script]
+  (let [init-fn (dom/data script :fn)
+        dom-ref (dom/data script :ref) 
+        args (dom/get-html script)
+        args (when (and args (not= "" args))
+               (reader/read-string args))
+        args (condp = dom-ref
+               "none" args
+               "self" (cons script args)
+               "parent" (cons (dom/get-parent script) args)
+               "previous-sibling" (cons (dom/get-previous-sibling script) args)
+               "next-sibling" (cons (dom/get-next-sibling script) args)
+               (throw (ex-info "script tag with invalid dom ref" {:dom-ref dom-ref :init-fn init-fn :script script})))]
+
+    (let [queued-fn (goog/getObjectByName init-fn)]
     (if queued-fn
-      (do (so/log "init" fn-id)
+      (do (so/log "init" init-fn)
           (apply queued-fn args))
-      (so/log "unknown init function" fn-id args))))
+      (so/log "unknown init function" init-fn args)))))
 
-(defn run-script-tags [module-name]
-  (doseq [script (dom/query (str "script[type=\"shadow/run\"][data-module=\"" module-name "\"]"))
-          :let [init-fn (dom/data script :fn)
-                dom-ref (dom/data script :ref) 
-                args (dom/get-html script)
-                args (when (and args (not= "" args))
-                       (reader/read-string args))
-                args (condp = dom-ref
-                       "none" args
-                       "self" (cons script args)
-                       "parent" (cons (dom/get-parent script) args)
-                       "previous-sibling" (cons (dom/get-previous-sibling script) args)
-                       "next-sibling" (cons (dom/get-next-sibling script) args)
-                       (throw (ex-info "script tag with invalid dom ref" {:dom-ref dom-ref :init-fn init-fn :script script})))]]
+(def modules (atom #{}))
 
-    (run-init-function init-fn args) 
-    ))
+(defn run-embedded-tags
+  "use after calling (dom/set-html node html) and that html may contain embedded script tags
+
+  only runs tags where the module is already loaded, if the module is not yet loaded to module-ready function
+  will pick remaining tags"
+  [node]
+  (doseq [script (dom/query "script[type=\"shadow/run\"]" node)]
+    (let [module (dom/data script :module)]
+      (when (contains? @modules module)
+        (run-script-tag script)))))
 
 (defn ^:export module-ready [module-name]
   (so/log "module-ready" module-name)
-  (run-script-tags module-name))
+  (swap! modules conj module-name)
+
+  ;; module finished loading, run all associated script tags
+  ;; eg. <script type="shadow/run" data-module="something" data-fn="my_ns.some_fn">args data (pr-str args) on server</script>
+  (doseq [script (dom/query (str "script[type=\"shadow/run\"][data-module=\"" module-name "\"]"))]
+    (run-script-tag script)
+    ))
 
 (defn ^:export module-error [module-name e]
   (so/log "module-error" module-name e))
