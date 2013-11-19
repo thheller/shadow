40d7154f4427c7a9abed53ddd0e3955a49a9f811
Thomas Heller <info@zilence.net>
2013-11-19 18:27:08 +0100

    rename project

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject shadow "0.1.0-SNAPSHOT"
+(defproject shadow/client "0.1.0-SNAPSHOT"
   :description "a cljs library for managed state in the shadows of the DOM"
   :url "http://github.com/thheller/shadow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  )
+  :dependencies [[org.clojure/clojure "1.5.1"]])
