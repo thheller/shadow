7727f0ff50e8aad597beae2e3425d0b68a47ee3c
Thomas Heller <info@zilence.net>
2013-12-15 19:07:35 +0100

    might as well make a release

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject shadow/client "0.1.0-SNAPSHOT"
+(defproject thheller/shadow-client "0.1.0"
   :description "a cljs library for managed state in the shadows of the DOM"
   :url "http://github.com/thheller/shadow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2080"]
-                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]]
+                 [org.clojure/clojurescript "0.0-2120"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]]
 
   :source-paths ["src/clj" "src/cljs"])
