631c7777cc31588e3dd947537ab1d90a90928053
Thomas Heller <th.heller@gmail.com>
2016-11-22 00:26:49 +0100

    bump

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject thheller/shadow-client "1.0.173"
+(defproject thheller/shadow-client "1.0.175"
   :description "a cljs library for managed state in the shadows of the DOM"
   :url "http://github.com/thheller/shadow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/core.async "0.2.371"]]
+  :dependencies [[org.clojure/core.async "0.2.395"]]
 
   :profiles {:dev
              {:source-paths ["dev"]
               :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                             [thheller/shadow-build "1.0.236"]
+                             [thheller/shadow-build "1.0.239"]
                              [org.clojure/clojurescript "1.9.293"]]}}
 
   :source-paths ["src/clj"
