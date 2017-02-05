93b0e8ae5dd89d7752e254a1b82e078ba7290ded
Thomas Heller <th.heller@gmail.com>
2017-02-05 18:36:54 +0100

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thheller/shadow-client "1.0.180"
+(defproject thheller/shadow-client "1.0.183"
   :description "a cljs library for managed state in the shadows of the DOM"
   :url "http://github.com/thheller/shadow"
   :license {:name "Eclipse Public License"
