f785972ae990ba3f52599b315c6c9b6169ffc922
Thomas Heller <th.heller@gmail.com>
2017-12-07 00:02:04 +0100

    react v16.2 fragment support

--- src/main/shadow/markup/react.cljs
+++ src/main/shadow/markup/react.cljs
@@ -4,7 +4,8 @@
   (:require-macros [shadow.markup.react :as m])
   ;; macro requires this
   (:require [shadow.markup.react.impl.interop :as interop]
-            [shadow.markup.css :as css]))
+            [shadow.markup.css :as css]
+            ["react" :as react]))
 
 (defn for [& args]
   (throw (ex-info "shadow.markup.react/for is a macro" {})))
@@ -16,5 +17,11 @@
   (let [html #js {"__html" html}]
     (js/goog.object.set props "dangerouslySetInnerHTML" html))
   props)
+
+(defn fragment [& body]
+  (let [arr (into-array body)]
+    (.unshift arr nil)
+    (.unshift arr react/Fragment)
+    (.apply react/createElement nil arr)))
 
 (m/define-elements)
