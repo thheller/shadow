313e2b8365380eddf33cabb0af8e0668cffb0e67
Thomas Heller <th.heller@gmail.com>
2018-03-20 15:56:24 +0100

    extendable defc experiment

--- src/main/shadow/react/component.clj
+++ src/main/shadow/react/component.clj
@@ -62,6 +62,12 @@
 
          (js/goog.object.set ~name "shadow$component" #(cljs.core/-deref ~cmp)))))
 
+(defmulti defc-extend (fn [cmp attr value] attr)
+  :default ::default)
+
+(defmethod defc-extend ::default [cmp attr value]
+  (assoc cmp attr value))
+
 (defmacro defc
   [& body]
   (let [data (s/conform ::factory-def body)]
@@ -95,8 +101,9 @@
             (fn [cmp [attr-type attr]]
               (case attr-type
                 :kv-pair
-                (-> (assoc cmp (:key attr) (:value attr))
-                    (add-ext (:key attr)))
+                (let [{:keys [key value]} attr]
+                  (-> (defc-extend cmp key value)
+                      (add-ext key)))
                 :fn
                 (let [{:keys [key args body]} attr
 
@@ -108,17 +115,12 @@
                       ns-alias
                       (ana/resolve-ns-alias &env (namespace key) key)
 
-                      ;; this might be a bad idea since it introduces a new local
-                      fn-id
-                      (symbol (str (namespace key) "$" (name key)))
-
                       fn-key
                       (keyword (name ns-alias) (name key))]
 
                   (-> cmp
-                      (assoc fn-key `(fn ~fn-id ~args ~@body))
-                      (add-ext key)
-                      ))))
+                      (defc-extend fn-key `(fn ~args ~@body))
+                      (add-ext key)))))
             {::type type}
             attrs)]
 
