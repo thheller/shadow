beab6586a9782ceb3e2a4bfa2935fb8756e13d2c
Thomas Heller <th.heller@gmail.com>
2020-06-06 19:46:43 +0200

    bump 1.3.3

--- project.clj
+++ project.clj
@@ -1,8 +1,12 @@
-(defproject thheller/shadow-client "1.3.2"
+(defproject thheller/shadow-client "1.3.3"
   :description "a cljs library for managed state in the shadows of the DOM"
   :url "http://github.com/thheller/shadow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :repositories
+  {"clojars" {:url "https://clojars.org/repo"
+              :sign-releases false}}
 
   :dependencies
   [[org.clojure/clojurescript "1.9.946" :scope "provided"]
